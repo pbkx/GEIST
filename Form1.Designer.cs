e6f93819a5876006dab142505dce462edcdc87e3
pbkx <nanomantis07@gmail.com>
2025-01-14 14:39:39 -0600

    Worked on search bar.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,25 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.logoImage = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.searchBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.logoImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // logoImage
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(52, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.logoImage.BackColor = System.Drawing.Color.Transparent;
+            this.logoImage.Image = ((System.Drawing.Image)(resources.GetObject("logoImage.Image")));
+            this.logoImage.Location = new System.Drawing.Point(52, 12);
+            this.logoImage.Name = "logoImage";
+            this.logoImage.Size = new System.Drawing.Size(50, 50);
+            this.logoImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoImage.TabIndex = 0;
+            this.logoImage.TabStop = false;
+            this.logoImage.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
@@ -59,21 +62,39 @@
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
-            // textBox1
+            // titleBox
             // 
-            this.textBox1.BackColor = System.Drawing.Color.Black;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Enabled = false;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.Transparent;
-            this.textBox1.Location = new System.Drawing.Point(26, 68);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.textBox1.Size = new System.Drawing.Size(100, 13);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "G E I S T";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.titleBox.BackColor = System.Drawing.Color.Black;
+            this.titleBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.titleBox.Enabled = false;
+            this.titleBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleBox.ForeColor = System.Drawing.Color.Transparent;
+            this.titleBox.Location = new System.Drawing.Point(26, 68);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.titleBox.Size = new System.Drawing.Size(100, 13);
+            this.titleBox.TabIndex = 2;
+            this.titleBox.Text = "G E I S T";
+            this.titleBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.titleBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // searchBox
+            // 
+            this.searchBox.BackColor = System.Drawing.Color.Black;
+            this.searchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.ForeColor = System.Drawing.Color.White;
+            this.searchBox.Location = new System.Drawing.Point(132, 3);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.searchBox.Size = new System.Drawing.Size(1067, 20);
+            this.searchBox.TabIndex = 4;
+            this.searchBox.TextChanged += new System.EventHandler(this.searchBox_TextChanged);
             // 
             // Form1
             // 
@@ -82,14 +103,15 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(1220, 639);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.logoImage);
             this.Controls.Add(this.pictureBox2);
             this.ForeColor = System.Drawing.Color.Transparent;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logoImage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -98,9 +120,11 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox logoImage;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox searchBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,19 @@ namespace GEIST
         public Form1()
         {
             InitializeComponent();
+
+            searchBox = new TextBox
+            {
+                Text = "Search here!",
+                ForeColor = Color.Gray,
+                Width = 1067,
+                Location = new Point(132, 3)
+            };
+
+            searchBox.Enter += searchBox_Enter;
+            searchBox.Leave += searchBox_Leave;
+
+            Controls.Add(searchBox);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -35,6 +48,29 @@ namespace GEIST
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void searchBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchBox_Enter(object sender, EventArgs e)
+        {
+            if (searchBox.Text == "Search here!")
+            {
+                searchBox.Text = "";
+                searchBox.ForeColor = Color.White;
+            }
+        }
+
+        private void searchBox_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(searchBox.Text))
+            {
+                searchBox.Text = "Search here!";
+                searchBox.ForeColor = Color.Gray;
+            }
         }
     }
 }
